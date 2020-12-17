cae0bf5943c6e8dcc33bd65d59c524434fd8cfe1
Nick Jansen <n.jansen@sitepilot.io>
2020-12-17 16:22:35 +0100

    Update php max execution time

--- filesystem/opt/sitepilot/templates/php/php.ini.blade.php
+++ filesystem/opt/sitepilot/templates/php/php.ini.blade.php
@@ -2,6 +2,7 @@ date.timezone="{{ $php['timezone'] }}"
 post_max_size={{ $php['uploadLimit'] }}M
 upload_max_filesize={{ $php['uploadLimit'] }}M
 memory_limit={{ $php['memoryLimit'] }}M
+max_execution_time={{ $php['maxExecutionTime'] }}
 max_input_vars=3000
 
 opcache.fast_shutdown=1
@@ -13,4 +14,5 @@ sendmail_path=/usr/bin/msmtp -C /opt/sitepilot/etc/msmtp/msmtp.conf --read-envel
 
 mail.log=/opt/sitepilot/app/logs/php_mail.log 
 error_log=/opt/sitepilot/app/logs/php_error.log
+
 disable_functions=
